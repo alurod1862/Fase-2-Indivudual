29340202bf358f6632ad320f3dd0645e7b22d1e1
alurod1862 <91328330+alurod1862@users.noreply.github.com>
2021-10-15 17:36:12 +0200

    Update Comandos-gandia-campus.ps1

--- Comandos-gandia-campus.ps1
+++ Comandos-gandia-campus.ps1
@@ -1,2 +1,24 @@
 #Comando para instalr active directory
 Install-WindowsFeature –Name AD-Domain-Services –IncludeManagementTools
+
+
+#Añadir dominio
+
+PS C:\> 
+Import-Module ADDSDeployment
+Install-ADDSDomain `
+-NoGlobalCatalog:$false `
+-CreateDnsDelegation:$true `
+-Credential (Get-Credential) `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThrshold" `
+-DomainType "ChildDomain" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NewDomainName "gandia" `
+-NewDomainNetbiosName "GANDIA" `
+-ParentDomainName "smr.local" `
+-NoRebootOnCompletion:$false `
+-SiteName "Default-First-Site-Name" `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
