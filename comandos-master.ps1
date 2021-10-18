674a831cfdef7bdca8028126667f32855cbcb87a
alurod1862 <91328330+alurod1862@users.noreply.github.com>
2021-10-18 11:31:03 +0200

    Update comandos-master.ps1

--- comandos-master.ps1
+++ comandos-master.ps1
@@ -1,13 +1,1 @@
-#caja de herraminetas
-desde herramientas  usuarios y equipos /upv.es/domaincontroles hay que borrar las que tiene - r
 
-
-
-luego 
-
-
-
-sitios y servicios de active directory/sites7Default first name/servers/master/borrar el que tiene r 
-
-
-despues comandos
